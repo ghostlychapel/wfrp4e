294adebaefabd32347e2fddbe2268acb919504f7
wwchappell <wwchappell@gmail.com>
2022-08-29 10:59:25 -0400

    Initial commit

---
+++ src/main/java/com/placeholder/wfrp4e/Wfrp4eApplication.java
@@ -0,0 +1,13 @@
+package com.placeholder.wfrp4e;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Wfrp4eApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Wfrp4eApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/placeholder/wfrp4e/Wfrp4eApplicationTests.java
@@ -0,0 +1,13 @@
+package com.placeholder.wfrp4e;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Wfrp4eApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
