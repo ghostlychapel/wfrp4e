475e770ed8304f6fa5463abfce2fa359727c5476
ghostlychapel <wwchappell@gmail.com>
2022-09-01 01:21:42 -0400

    added d100 logic and combined d10 and d100 to main application file

--- src/main/java/com/springboot/wfrp4e/RollDice.java
+++ src/main/java/com/springboot/wfrp4e/RollDice.java
@@ -7,6 +7,8 @@ public class RollDice {
 
     public List rollD10() {
 
+        System.out.println("How many dice would you like to roll?");
+
         int userDiceChoice;
         List<Integer> diceRollList = new ArrayList<>();
 
@@ -19,17 +21,24 @@ public class RollDice {
             diceRollListTotal += x;
         }
 
-
         System.out.println("Your dice roll total is " + diceRollListTotal + "\nYour dice rolls were as follows: \n" + diceRollList);
 
         return diceRollList;
     }
 
+    public int rollD100() {
+
+        int diceRoll10s = (random1to10()*10);
+        int diceRoll1s = random1to10();
+        int diceRoll100 = Math.addExact(diceRoll10s, diceRoll1s);
+        System.out.println("Your roll for d100 is " + diceRoll100 + "\nYour dice rolls were as follows: \n" + "The 10's die rolled for " + diceRoll10s + ". And the 1's die rolled for " + diceRoll1s);
+
+        return diceRoll100;
+    }
+
     private int random1to10() {
 
         int diceRoll = (int) (Math.floor(Math.random() * 10) +1);
-
-        //System.out.print(diceRoll);
 
         return diceRoll;
     }
--- src/main/java/com/springboot/wfrp4e/Wfrp4eApplication.java
+++ src/main/java/com/springboot/wfrp4e/Wfrp4eApplication.java
@@ -3,6 +3,8 @@ package com.springboot.wfrp4e;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Scanner;
+
 @SpringBootApplication
 public class Wfrp4eApplication {
 
@@ -12,9 +14,21 @@ public class Wfrp4eApplication {
 
 		System.out.println("Would you like to roll for for d100 or d10s?");
 
-		RollDice rollDice = new RollDice();
-		rollDice.rollD10();
+		String userD10orD100;
 
+		Scanner userInputD10orD100 = new Scanner(System.in);
+		userD10orD100 = userInputD10orD100.nextLine();
+
+		if (userD10orD100 .equalsIgnoreCase("d10s")) {
+			RollDice rollDice = new RollDice();
+			rollDice.rollD10();
+		}
+		else if (userD10orD100 .equalsIgnoreCase("d100")){
+			RollDice rollDice = new RollDice();
+			rollDice.rollD100();
+		}
+		else {
+			System.out.println("Please type \"d100\" or \"d10s\" ");
+		}
 	}
-
 }
